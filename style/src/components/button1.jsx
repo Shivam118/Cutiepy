aa343de4e5d167c5632512c3574b06f4d0a8fab7
Keshav888 <tiwarikeshav88820@gmail.com>
2022-03-22 22:24:55 +0530

    3 Buttons in React

---
+++ style/src/components/button1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/button1.css';
+
+const button1 = () => {
+  return (
+    <div className="button1">
+        <a href="#" className="button1Link">Hover Me</a>
+    </div>
+  )
+}
+
+export default button1
---
+++ style/src/components/button2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/button2.css';
+
+const button2 = () => {
+  return (
+    <div className="button2">
+        <a href="#">button</a>
+    </div>
+  )
+}
+
+export default button2
